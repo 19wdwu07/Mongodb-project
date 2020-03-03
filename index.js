d50d1f05d2b5ec04dd155e1ca2e6d0b89e169a3c
19wdwu07 <beulathamban@gmail.com>
2020-03-03 05:25:59 +0000

    removed the uri string

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const config = require('./config.json');
 
 const port = 3000;
 //connect to db
-// const mongodbURI = 'mongodb+srv://beulasamuel:<password>@beudb-sxyu7.mongodb.net/test?retryWrites=true&w=majority';
+
 const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}-sxyu7.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log('DB connected!'))
