1247dea43ff8fc62b2338ee5802c893888504b2b
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-03 01:37:55 +0000

    mongodb connected

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcryptjs = require('bcryptjs');
+const config = require('./config.json');
+
+const port = 3000;
+//connect to db
+// const mongodbURI = 'mongodb+srv://beulasamuel:<password>@beudb-sxyu7.mongodb.net/test?retryWrites=true&w=majority';
+const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}-sxyu7.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('DB connected!'))
+.catch(err =>{
+  console.log(`DBConnectionError: ${err.message}`);
+});
+
+//test the connectivity
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('We are connected to mongo db');
+});
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
